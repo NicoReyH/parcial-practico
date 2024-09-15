65e33c4abbc4ac2bf73ddc6c4ab4772698aa7375
Nicolas Rey <nicolasreydh@gmail.com>
2024-09-15 11:55:42 -0500

    Add entities and relations

---
+++ src/supermarket/supermarket.entity/supermarket.entity.ts
@@ -0,0 +1,23 @@
+import { Column, Entity, ManyToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { CityEntity } from '../../city/city.entity/city.entity';
+
+@Entity()
+export class SupermarketEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  longitude: number;
+
+  @Column()
+  latitude: number;
+
+  @Column()
+  website: string;
+
+  @ManyToMany(() => CityEntity, (city) => city.supermarkets)
+  cities: CityEntity[];
+}
